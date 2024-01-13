4b7906b241e45328c7683a62037b9f99d9455b17
Dmytro-Diumin <microb201196@gmail.com>
2024-01-13 21:13:50 +0200

    added task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const btnChangeColor = document.querySelector('.change-color');
+const widget = document.querySelector('.widget');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  widget.style.backgroundColor = randomColor;
+  spanColor.textContent = randomColor;
+});
